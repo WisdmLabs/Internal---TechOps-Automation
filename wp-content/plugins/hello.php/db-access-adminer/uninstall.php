2303197499347d11abe210cf48459216fa5cb17c
chaudhari-mithilesh <mithilesh.chaudhari@wisdmlabs.com>
2025-04-15 10:01:41 +0000

    cleanup: Remove all content except techops-content-sync plugin

--- wp-content/plugins/hello.php/db-access-adminer/uninstall.php
+++
@@ -1,2 +0,0 @@
-<?php
-defined('WP_UNINSTALL_PLUGIN') && delete_option(basename(WP_UNINSTALL_PLUGIN, '.php'));
--- wp-content/plugins/hello.php/otter-blocks/build/blocks/vendor.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array(), 'version' => '0e5ae7c8fe01bee2101b');
