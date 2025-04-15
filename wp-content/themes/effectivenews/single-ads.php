f9c4327825e6c746e0700c4617385ebcafa249cf
chaudhari-mithilesh <mithilesh.chaudhari@wisdmlabs.com>
2025-04-15 10:38:44 +0000

    chore: Remove all plugins except techops-content-sync

--- wp-content/themes/effectivenews/single-ads.php
+++
@@ -1,9 +0,0 @@
-<?php get_header(); ?>
-<div class="ads-preview-wrap">
-<?php
-    global $post;
-    echo do_shortcode('[ad id="'.$post->ID.'"]');
-    echo do_shortcode('[gap height="10"]');
-?>
-</div>
-<?php get_footer(); ?>
