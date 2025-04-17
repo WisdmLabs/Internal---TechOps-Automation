ad7310628e4ac57eb04e2a98a4d3e8c1c8521f36
chaudhari-mithilesh <chaudhari-mithilesh@users.noreply.github.com>
2025-04-17 17:27:22 +0000

    [create-pull-request] automated change

---
+++ wp-content/plugins/wordpress-seo/wordpress-seo/vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-trunk',
+        'version' => 'dev-trunk',
+        'type' => 'wordpress-plugin',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => 'd277f8e8f7af89ac99ca3fcc198d793671923bbd',
+        'name' => 'yoast/wordpress-seo',
+        'dev' => false,
+    ),
+    'versions' => array(
+        'composer/installers' => array(
+            'pretty_version' => 'v2.3.0',
+            'version' => '2.3.0.0',
+            'type' => 'composer-plugin',
+            'install_path' => __DIR__ . '/./installers',
+            'aliases' => array(),
+            'reference' => '12fb2dfe5e16183de69e784a7b84046c43d97e8e',
+            'dev_requirement' => false,
+        ),
+        'yoast/wordpress-seo' => array(
+            'pretty_version' => 'dev-trunk',
+            'version' => 'dev-trunk',
+            'type' => 'wordpress-plugin',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => 'd277f8e8f7af89ac99ca3fcc198d793671923bbd',
+            'dev_requirement' => false,
+        ),
+    ),
+);
